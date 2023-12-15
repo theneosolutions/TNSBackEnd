5bd25647f756104f13a3058b428ea1c9bb35cd45
MZainMalik <149575690+MZainMalik@users.noreply.github.com>
2023-12-15 12:49:36 +0500

    update

---
+++ src/api/socail/controllers/socail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * socail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::socail.socail');
---
+++ src/api/socail/routes/socail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * socail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::socail.socail');
